ee648c5f3fb6652f595095201acc4e685d093123
Coskun Uyar <coskunuyarr@gmail.com>
2020-06-15 02:17:19 +0300

    comment type is added

--- schema/schema.js
+++ schema/schema.js
@@ -25,7 +25,19 @@ const postList = [
     {id: "7" , ownerId: "3" , likedByUsers: [] , img: "https://source.unsplash.com/random"},
     {id: "8" , ownerId: "3" , likedByUsers: ["1","2"] , img: "https://source.unsplash.com/random"},
     {id: "9" , ownerId: "3" , likedByUsers: ["2","3"] , img: "https://source.unsplash.com/random"}
-]
+];
+
+const commentList = [
+    {id:"1" , ownerId: "1" , postId: "1" , text: "Perfect Pic!"},
+    {id:"2" , ownerId: "2" , postId: "5" , text: "Perfect Pic!"},
+    {id:"3" , ownerId: "3" , postId: "6" , text: "Perfect Pic!"},
+    {id:"4" , ownerId: "1" , postId: "7" , text: "Perfect Pic!"},
+    {id:"5" , ownerId: "3" , postId: "7" , text: "Perfect Pic!"},
+    {id:"6" , ownerId: "2" , postId: "7" , text: "Perfect Pic!"},
+    {id:"7" , ownerId: "3" , postId: "5" , text: "Perfect Pic!"},
+    {id:"8" , ownerId: "1" , postId: "3" , text: "Perfect Pic!"},
+    {id:"9" , ownerId: "1" , postId: "3" , text: "Perfect Pic!"},
+];
 
 const UserType = new GraphQLObjectType({
     name: 'User',
@@ -52,7 +64,7 @@ const PostType = new GraphQLObjectType({
     fields: () => ({
         id: { type: GraphQLID },
         img: { type: GraphQLString },
-        likedBy: {
+        likes: {
             type: new GraphQLList(UserType),
             resolve(parent,args){
                 return userList.filter( user =>  parent.likedByUsers.includes(user.id));
@@ -64,6 +76,18 @@ const PostType = new GraphQLObjectType({
                 return userList.filter( user =>  parent.likedByUsers.includes(user.id)).length;
             }
         },
+        comments: {
+            type: new GraphQLList(CommentType),
+            resolve(parent,args){
+                return commentList.filter(comment => comment.postId === parent.id);
+            }
+        },
+        numberOfComments: {
+            type: GraphQLInt,
+            resolve(parent,args){
+                return commentList.filter(comment => comment.postId === parent.id).length;
+            }
+        },
         owner: {
             type: UserType,
             resolve(parent,args){
@@ -72,6 +96,26 @@ const PostType = new GraphQLObjectType({
         }
     })
 });
+
+const CommentType = new GraphQLObjectType({
+    name: 'Comment',
+    fields: () => ({
+        id: { type: GraphQLID },
+        text: { type: GraphQLString },
+        owner: {
+            type: UserType,
+            resolve(parent,args){
+                return userList.filter( user => user.id === parent.ownerId )[0];
+            }
+        },
+        post: {
+            type: PostType,
+            resolve(parent , args){
+                return postList.filter( post =>  post.id === parent.postId )[0];
+            }
+        }
+    })
+})
 
 
 const RootQuery = new GraphQLObjectType({
@@ -88,6 +132,33 @@ const RootQuery = new GraphQLObjectType({
             type: new GraphQLList(UserType),
             resolve(parent , args){
                 return userList;
+            }
+        },
+        post: {
+            type: PostType,
+            args: {id: { type: GraphQLID}},
+            resolve(parent,args){
+                return postList.filter(post => post.id === args.id)[0];
+            }
+        },
+        posts: {
+            type: new GraphQLList(PostType),
+            resolve(parent,args){
+                return postList;
+            }
+        },
+        comment: {
+            type: CommentType,
+            args: {id: { type: GraphQLID}},
+            resolve(parent,args){
+                return commentList.filter(post => post.id === args.id)[0];
+            }
+        },
+        comments: {
+            type: new GraphQLList(CommentType),
+            args: {postId: { type: GraphQLID}},
+            resolve(parent,args){
+                return commentList.filter( comment => comment.postId === args.postId);
             }
         }
     }
