ba092021d564dde1d9c0f2618f0ea9cb8285880a
Coskun Uyar <coskunuyarr@gmail.com>
2020-07-09 18:16:58 +0300

    cors is added for local testing

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const mongoose = require('mongoose');
 const schema = require('./graph/index.js');
+const cors = require('cors');
 const { mongoURL } = require('./secret');
 
 
@@ -9,5 +10,6 @@ mongoose.connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connection.once('open' , () => console.log('Connected to database.'));
 
 const app = express();
+app.use(cors());
 app.use('/v1',graphqlHTTP({ schema, graphiql: true }));
 app.listen(4000, () => console.log(`Application is running on port 4000`));
